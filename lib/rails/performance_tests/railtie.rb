30bf8d9e5f4f939c6a61f21e4c16ad6aebf9f441
Yves Senn <yves.senn@gmail.com>
2013-01-05 23:58:08 +0100

    add performance testing rake tasks

--- lib/rails/performance_tests/railtie.rb
+++ lib/rails/performance_tests/railtie.rb
@@ -4,6 +4,10 @@ module Rails
   module PerformanceTest
     class Railtie < ::Rails::Railtie
 
+      rake_tasks do
+        load 'rails/performance_tests/railties/testing.tasks'
+      end
+
       initializer "action_dispatch.performance_test" do
         ActiveSupport.on_load(:action_controller) do
           require "rails/performance_tests/action_dispatch"
