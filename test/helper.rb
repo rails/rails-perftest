cb06b1d28920f0b926b0da1976dbb5cd89fd521c
Yves Senn <yves.senn@gmail.com>
2015-04-24 12:07:43 +0200

    build fix, only assign `test_order` if available.

--- test/helper.rb
+++ test/helper.rb
@@ -6,4 +6,4 @@ require 'active_support'
 
 # Show backtraces for deprecated behavior for quicker cleanup.
 ActiveSupport::Deprecation.debug = true
-ActiveSupport::TestCase.test_order = :random
+ActiveSupport::TestCase.test_order = :random if ActiveSupport::TestCase.respond_to?(:test_order=)
